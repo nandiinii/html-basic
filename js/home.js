a04cdde4d242c67b7f91bea5e5f22d833dfa1a2f
nandiinii <99135337+nandiinii@users.noreply.github.com>
2023-02-21 22:04:48 +0530

    Committing home.js

---
+++ js/home.js
@@ -0,0 +1,18 @@
+var str = 'string value';
+console.log(str);
+var num = 3;
+console.log(num);
+var bool = true;
+console.log(bool);
+var arr = ['a',2,true];
+console.log(arr);
+
+function fullName(firstName, lastName) {
+    var fullName = firstName + ' ' +  lastName;
+    return fullName; 
+}
+var fullName = fullName('Preetha', 'Nandini');
+console.log(fullName.toUpperCase());
+
+
+//Concatenation of strings
